c38a32a990a3f2f48a9815099261b53190212cc9
Njål Sørland <njaalis2000@gmail.com>
2023-02-22 09:51:22 +0100

    remove (hopefully) obsolete code

--- mapConverter.py
+++ mapConverter.py
@@ -11,8 +11,6 @@ class MapConverter:
         self.nodes = None
         self.ways = None
         self.roads = None
-        self.smallestX = None
-        self.smallestY = None
 
     # ------------------ Read the data ------------------
 
@@ -34,34 +32,16 @@ class MapConverter:
 
     # ------------------ Process the data ------------------
 
-    def count_nodes(self):
-        nodes = self.nodes
-        ways = self.ways
-
-        # count the number of times a node is used
-        for id in nodes:
-            nodes[id]["count"] = 0
-
-        for id in ways:
-            path = ways[id]
-            for node in path:
-                if node in nodes:
-                    if node == path[0] or node == path[-1]:
-                        nodes[node]["count"] += 1
-                    else:
-                        nodes[node]["count"] += 2
-
     def convert_to_coordinates(self):
         nodes = self.nodes
         ways = self.ways
 
         # convert the nodes to x and y coordinates and create a list of roads
-        roads = {}
+        roads = []
         for id in tqdm(ways):
             path = ways[id]
             x = []
             y = []
-            counts = []
             for node in path:
                 lon = (nodes[node]["lon"])
                 lat = (nodes[node]["lat"])
@@ -69,57 +49,19 @@ class MapConverter:
                 x1, y1 = transformer.transform(lon, lat)
                 x.append(x1)
                 y.append(y1)
-                counts.append(nodes[node]["count"])
-            roads[id] = {"nodes": [x, y], "counts": counts}
+            roads.append([x, y])
 
         # find the smallest x and y values and subtract them from all x and y values
-        smallestX = min([min(roads[id]["nodes"][0]) for id in roads])
-        smallestY = min([min(roads[id]["nodes"][1]) for id in roads])
+        smallestX = min([min(road[0]) for road in roads])
+        smallestY = min([min(road[1]) for road in roads])
 
-        self.smallestX = smallestX
-        self.smallestY = smallestY
-
-        for id in roads:
-            roads[id]["nodes"][0] = [
-                x - smallestX for x in roads[id]["nodes"][0]]
-            roads[id]["nodes"][1] = [
-                y - smallestY for y in roads[id]["nodes"][1]]
+        for i in range(len(roads)):
+            roads[i][0] = [
+                x - smallestX for x in roads[i][0]]
+            roads[i][1] = [
+                y - smallestY for y in roads[i][1]]
 
         self.roads = roads
-
-    def merge_roads(self):
-        roads = self.roads
-
-        # merge roads that are connected and have a count of 2
-        for id in tqdm(roads.copy()):
-            if id not in roads:
-                continue
-            if roads[id]["counts"][0] == 2:
-                for id2 in roads.copy():
-                    if roads[id]["nodes"][0][0] == roads[id2]["nodes"][0][-1] and roads[id]["nodes"][1][0] == roads[id2]["nodes"][1][-1]:
-                        roads[id]["nodes"][0] = roads[id2]["nodes"][0] + \
-                            roads[id]["nodes"][0]
-                        roads[id]["nodes"][1] = roads[id2]["nodes"][1] + \
-                            roads[id]["nodes"][1]
-                        roads[id]["counts"] = roads[id2]["counts"] + \
-                            roads[id]["counts"]
-                        del roads[id2]
-            if roads[id]["counts"][-1] == 2:
-                for id2 in roads.copy():
-                    if roads[id]["nodes"][0][-1] == roads[id2]["nodes"][0][0] and roads[id]["nodes"][1][-1] == roads[id2]["nodes"][1][0]:
-                        roads[id]["nodes"][0] = roads[id]["nodes"][0] + \
-                            roads[id2]["nodes"][0]
-                        roads[id]["nodes"][1] = roads[id]["nodes"][1] + \
-                            roads[id2]["nodes"][1]
-                        roads[id]["counts"] = roads[id]["counts"] + \
-                            roads[id2]["counts"]
-                        del roads[id2]
-
-    def get_intersections(self):
-        nodes = self.nodes
-        # get all nodes with a count higher than 2
-        intersections = [node for node in nodes if nodes[node]["count"] > 2]
-        return intersections
 
     # ------------------ Plot the data ------------------
 
@@ -127,50 +69,20 @@ class MapConverter:
         nodes = self.nodes
         roads = self.roads
 
-        roadParts = [road["nodes"] for road in roads.values()]
-        roads = roadParts
-
         # plot all roads
         for road in roads:
             plt.plot(road[1], road[0], 'k-')
-
-        # plot all nodes with color depending on count
-        for id in nodes:
-            lon = (nodes[id]["lon"])
-            lat = (nodes[id]["lat"])
-            transformer = Transformer.from_crs("EPSG:4326", "EPSG:3857")
-            x1, y1 = transformer.transform(lon, lat)
-            x1 -= self.smallestX
-            y1 -= self.smallestY
-            if nodes[id]["count"] == 1:
-                plt.plot(y1, x1, 'ro')
-            elif nodes[id]["count"] == 2:
-                plt.plot(y1, x1, 'go')
-            else:
-                plt.plot(y1, x1, 'bo')
         plt.show()
 
     # ------------------ Store the data ------------------
     def store_data(self, filename):
-        nodes = self.nodes
         roads = self.roads
-        intersections = self.get_intersections()
-        smallestX = self.smallestX
-        smallestY = self.smallestY
 
         # write to file in formattedMaps folder
         with open(f'formattedMaps/{filename}.txt', 'w') as f:
             # clear the file if it exists
             f.truncate(0)
-            f.write(str(len(intersections)) + " " + str(len(roads))+"\n")
-            for intersection in intersections:
-                lon = (nodes[intersection]["lon"])
-                lat = (nodes[intersection]["lat"])
-                transformer = Transformer.from_crs("EPSG:4326", "EPSG:3857")
-                x1, y1 = transformer.transform(lon, lat)
-                x1 -= smallestX
-                y1 -= smallestY
-                f.write(str(x1) + " " + str(y1)+"\n")
+            f.write(str(str(len(roads))+"\n"))
             y = [road[0] for road in roads if not type(road) == int]
             x = [road[1] for road in roads if not type(road) == int]
             for i in range(len(x)):
@@ -180,14 +92,10 @@ class MapConverter:
 
     def create_map(self):
         self.read_data()
-        self.count_nodes()
         self.convert_to_coordinates()
-        # self.merge_roads()
-        self.get_intersections()
+        self.plot_data()
 
 
 if __name__ == "__main__":
-    map = MapConverter("maps/mapKatta.json")
+    map = MapConverter("maps/circleHell.json")
     map.create_map()
-    map.plot_data()
-    print(map.roads)
